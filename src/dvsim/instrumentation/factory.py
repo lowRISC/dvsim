a3e8e94f3767f1e54c2036eaea8405ec6fb1ae12
Alex Jones <alex.jones@lowrisc.org>
2026-02-16 15:10:52 +0000

    feat: add scheduler instrumentation base classes
    Implement the base class functionality for scheduler instrumentation,
    where new instrumentation options can be added by subclassing the
    `SchedulerInstrumentation` and registering the class with the
    `InstrumentationFactory` registry. This allows DVSim to potentially be
    extended to add custom scheduler instrumentation logic.
    
    Signed-off-by: Alex Jones <alex.jones@lowrisc.org>

---
+++ src/dvsim/instrumentation/factory.py
@@ -0,0 +1,48 @@
+# Copyright lowRISC contributors (OpenTitan project).
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+"""DVSim scheduler instrumentation factory."""
+
+from typing import ClassVar
+
+from dvsim.instrumentation.base import (
+    CompositeInstrumentation,
+    NoOpInstrumentation,
+    SchedulerInstrumentation,
+)
+from dvsim.instrumentation.metadata import MetadataInstrumentation
+
+__all__ = ("InstrumentationFactory",)
+
+
+class InstrumentationFactory:
+    """Factory/registry for scheduler instrumentation implementations."""
+
+    _registry: ClassVar[dict[str, type[SchedulerInstrumentation]]] = {}
+
+    @classmethod
+    def register(cls, name: str, constructor: type[SchedulerInstrumentation]) -> None:
+        """Register a new scheduler instrumentation type."""
+        cls._registry[name] = constructor
+
+    @classmethod
+    def options(cls) -> list[str]:
+        """Get a list of available scheduler instrumentation types."""
+        return list(cls._registry.keys())
+
+    @classmethod
+    def create(cls, names: list[str] | None) -> SchedulerInstrumentation:
+        """Create a scheduler instrumentation of the given types.
+
+        Arguments:
+            names: A list of registered instrumentation names to compose into a single
+            instrumentation object, or None / an empty list for no instrumentation.
+
+        """
+        if not names:
+            return NoOpInstrumentation()
+
+        instances: list[SchedulerInstrumentation] = [MetadataInstrumentation()]
+        instances.extend([cls._registry[name]() for name in names])
+        return CompositeInstrumentation(instances)
