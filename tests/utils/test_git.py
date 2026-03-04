f1bd71c64cd3b4c41b817182b7a6cbb6111e3401
Alex Jones <alex.jones@lowrisc.org>
2026-03-04 15:53:49 +0000

    feat: add short option to git commit hash retrieval
    Signed-off-by: Alex Jones <alex.jones@lowrisc.org>

--- tests/utils/test_git.py
+++ tests/utils/test_git.py
@@ -53,3 +53,17 @@ class TestGit:
             git_commit_hash(tmp_path),
             equal_to(r.head.commit.hexsha),
         )
+
+    @staticmethod
+    def test_git_short_commit_hash(tmp_path: "Path") -> None:
+        """Test that the expected shortened git commit sha is returned."""
+        r = Repo.init(path=tmp_path)
+
+        file = tmp_path / "a"
+        file.write_text("file to commit")
+        r.index.add([file])
+        r.index.commit("initial commit")
+
+        assert_that(
+            git_commit_hash(tmp_path, short=True), equal_to(r.git.rev_parse(r.head, short=True))
+        )
