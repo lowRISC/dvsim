3ea3a73b9b65fedbbbf94528be04943f892b0c4c
Alex Jones <alex.jones@lowrisc.org>
2026-05-13 11:52:49 +0100

    feat: add time formatting functions for metric display
    Add a couple of extra utilities for formatting time strings. These are
    intended to be used for the instrumentation reporting logic, but might
    be useful elsewhere as they are general enough.
    
    This constructs some time formatted strings that - as far as I can tell
    - cannot be trivially constructed using e.g. `strftime` in the standard
    `datetime` library. Specifically with regards to omitting leading zero
    hour/minute values. We could call strftime and strip the trailing prefix
    but that felt about as complicated as implementing our own utilities, so
    I just added it here. This functionality is exposed by some common
    third-party dependencies, but we don't already depend on these and I'd
    rather not introduce a large dependency just for this function.
    
    Signed-off-by: Alex Jones <alex.jones@lowrisc.org>

--- src/dvsim/utils/time.py
+++ src/dvsim/utils/time.py
@@ -12,3 +12,48 @@ TS_FORMAT_LONG = "%A %B %d %Y %H:%M:%S UTC"
 
 # Timestamp format for Hours:Minutes:Seconds display as used by the scheduler
 TS_HMS_FORMAT = "%H:%M:%S"
+
+
+def format_time_as_hms(seconds: float, *, decimals: int = 2, omit_zero: bool = False) -> str:
+    """Format a time in seconds like '12h 34m 56.79s'.
+
+    Args:
+        seconds: The time in seconds to format.
+        decimals: The number of decimal places to use for non-integer seconds (default 2).
+        omit_zero: True if zero fields (e.g. '0h 0m') should be omitted, false otherwise.
+
+    Returns:
+        A formatted time string.
+
+    """
+    if seconds < 0:
+        msg = f"Cannot format negative time value: {seconds}s"
+        raise ValueError(msg)
+
+    hours, remainder = divmod(seconds, 3600)
+    minutes, secs = divmod(remainder, 60)
+    hms = f"{secs:.{decimals}f}s"
+    if minutes > 0 or hours > 0 or not omit_zero:
+        hms = f"{int(minutes)}m " + hms
+    if hours > 0 or not omit_zero:
+        hms = f"{int(hours)}h " + hms
+    return hms
+
+
+def format_time_metric(
+    seconds: float, *, hms_decimals: int = 2, second_decimals: int = 2, omit_zero: bool = False
+) -> str:
+    """Return a time metric formatted as e.g. '2h 15m 37.21s (8,137.21s)'.
+
+    Args:
+        seconds: The time in seconds to format.
+        hms_decimals: The number of decimal places to use for the 'Xh Ym Zs' output.
+        second_decimals: The number of decimal places to use for the '(Xs)' output.
+        omit_zero: True if zero fields (e.g. '0h 0m') should be omitted, false otherwise.
+
+    Returns:
+        A formatted string for the given time metric value.
+
+    """
+    hms = format_time_as_hms(seconds, decimals=hms_decimals, omit_zero=omit_zero)
+    return f"{hms} ({seconds:,.{second_decimals}f}s)"
