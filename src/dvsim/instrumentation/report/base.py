c555215247256fc9d5b0594389d165a982423f21
Alex Jones <alex.jones@lowrisc.org>
2026-05-13 14:13:30 +0100

    feat: add base instrumentation report utilities & constants
    Add some base helpers/utilities & constants to assist in creating
    visualizations for the instrumentation report, to avoid duplication
    across added visualizations and increase reporting consistency.
    
    Signed-off-by: Alex Jones <alex.jones@lowrisc.org>

--- src/dvsim/instrumentation/report/base.py
+++ src/dvsim/instrumentation/report/base.py
@@ -4,24 +4,44 @@
 
 """DVSim scheduler instrumentation reporting & visualizations."""
 
-from collections.abc import Sequence
+from collections.abc import Iterable, Mapping, Sequence
 from enum import Enum
 from pathlib import Path
-from typing import Protocol
+from typing import Any, Protocol, TypeVar
 
+import plotly.graph_objects as go
 import plotly.offline
 from typing_extensions import Self
 
 from dvsim.instrumentation import InstrumentationResults
+from dvsim.instrumentation.records import JobInstrumentationMetadata
 from dvsim.logging import log
 from dvsim.report.artifacts import ReportArtifacts, render_static_content
 from dvsim.templates.render import render_template
 
 __all__ = (
+    "DEFAULT_VISUALIZATION_HEIGHT_PX",
+    "PLOTLY_TIMING_AXIS_CONFIG",
     "InstrumentationVisualizer",
     "RenderProfile",
+    "make_job_metadata_hover",
+    "make_repeating_color_map",
     "render_html_report",
+    "render_plotly_figure",
 )
+
+# The default figure height limit in pixels that visualizations should target, if possible.
+DEFAULT_VISUALIZATION_HEIGHT_PX: int = 1000
+
+# Standard plotly timing axis/tick config options
+PLOTLY_TIMING_AXIS_CONFIG: dict[str, Any] = {
+    "title": "Time (s)",
+    "tickformat": ",",
+    "ticks": "outside",
+    "tickwidth": 1,
+    "ticklen": 4,
+    "tickcolor": "black",
+}
 
 
 class RenderProfile(Enum):
@@ -130,3 +150,90 @@ def render_html_report(
             (outdir / plotly_js_path).write_text(artifacts[plotly_js_path])
 
     return artifacts
+
+
+def render_plotly_figure(fig: go.Figure, *args: list[Any], **kwargs: dict[str, Any]) -> str:
+    """Render a plotly figure / visualization to a HTML fragment to insert into a report.
+
+    Args:
+        fig: The plotly figure to render.
+        args: Any extra arguments to pass to `fig.to_html(...)`.
+        kwargs: Any extra keyword arguments to pass to `fig.to_html(...)`.
+
+    Returns:
+        The HTML fragment corresponding to the plotly Figure, without plotly.js included.
+
+    """
+    return fig.to_html(*args, full_html=False, include_plotlyjs=False, **kwargs)
+
+
+def make_job_metadata_hover(
+    job_id: str,
+    extra: Iterable[str] | Mapping[str, str] | None,
+    metadata: JobInstrumentationMetadata | None,
+    *,
+    omit_status: bool = False,
+) -> str:
+    """Make tooltip hover text for a graph item corresponding to some DVSim job.
+
+    Args:
+        job_id: The ID / full name of the job (displayed as the title).
+        extra: Any extra lines or key/value mapping to insert after the title & before metadata.
+        metadata: Any optional metadata to include at the end of the tooltip.
+        omit_status: A flag to disable display of status metadata. Useful for combined job views,
+          where jobs may have inconsistent statuses (e.g. flaky results).
+
+    Returns:
+        The tooltip hover HTML text for the job, intended to be used with plotly's `hovertemplate`.
+
+    """
+    lines = [f"<b>{job_id}</b>"]
+
+    if extra is not None:
+        if isinstance(extra, Mapping):
+            for key, value in extra.items():
+                lines.append(f"{key}: {value}")
+        else:
+            lines += list(extra)
+
+    if metadata is not None:
+        block_info = metadata.block
+        if metadata.block_variant:
+            block_info += f" ({metadata.block_variant})"
+        lines += [
+            "------------------",
+            f"Name: {metadata.name}",
+            f"Tool: {metadata.tool}",
+            f"Block: {block_info}",
+            f"Target: {metadata.target}",
+        ]
+        if not omit_status:
+            lines.append(f"Status: {metadata.status.value}")
+        if metadata.backend is not None:
+            lines.append(f"Backend: {metadata.backend}")
+
+    return "<br>".join(lines)
+
+
+T = TypeVar("T")
+
+
+def make_repeating_color_map(data: Iterable[str], colors: Iterable[T]) -> dict[str, T]:
+    """Make a color mapping that repeats for as long as is needed to fit the amount of data.
+
+    Args:
+        data: The category strings to map to the colors.
+        colors: The non-empty color items to be mapped. Repeated if needed to match `data`.
+
+    Returns:
+        The mapping from data items -> color, in a cyclically repeating order.
+
+    """
+    data = list(data)
+    colors = list(colors)
+    if len(colors) == 0:
+        raise ValueError("Given an empty list of colors to repeat?")
+
+    while len(colors) < len(data):
+        colors += colors.copy()
+    return dict(zip(data, colors, strict=False))
