8c48657d138cad212a3a27deefb866915e4ea294
Rupert Swarbrick <rswarbrick@lowrisc.org>
2026-05-21 12:12:43 +0100

    refactor: Give proper types to Testpoint.map_test_results
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- src/dvsim/testplan.py
+++ src/dvsim/testplan.py
@@ -217,7 +217,7 @@ class Testpoint(Element):
 
         self.tests = resolved_tests
 
-    def map_test_results(self, test_results) -> None:
+    def map_test_results(self, test_results: list[Result]) -> None:
         """Map test results to tests against this testpoint.
 
         Given a list of test results find the ones that match the tests listed
@@ -237,7 +237,6 @@ class Testpoint(Element):
             return
 
         for tr in test_results:
-            assert isinstance(tr, Result)
             if tr.name in self.tests:
                 tr.mapped = True
                 self.test_results.append(tr)
