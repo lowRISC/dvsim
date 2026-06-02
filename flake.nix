6783bc02938f49581f6ffe738c6da75796941562
James McCorrie <james.mccorrie@lowrisc.org>
2026-06-02 11:29:26 +0100

    chore: migrate flake to nixos 26.05 + nix flake update
    Signed-off-by: James McCorrie <james.mccorrie@lowrisc.org>

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   description = "DVSim development environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     flake-utils.url = "github:numtide/flake-utils";
