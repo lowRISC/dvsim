858050cbf831695e01770e71aef6dbd81de52efd
Neil Webb <nsw@lowrisc.org>
2026-07-07 14:48:23 +0200

    feat: Adds CDC results parsing for Meridian in the CDC flow
    This commit adds parsing of the Meridian CDC reports. The parser is
    using the meridiancdc.py file in the tools directory, so the parser in
    the local repo is not required, however, if the report_cmd variable is
    set in the hjson this will be overridden and the parser called by make.
    
    NOTE: the buckets have changed, adding waived and tofix, so the
    corresponding common_cdc_cfg.hjson file should be updated in line. This
    required a fix to msg_buckets.py to avoid an exception.
    
    Signed-off-by: Neil Webb <nsw@lowrisc.org>

--- src/dvsim/flow/cdc.py
+++ src/dvsim/flow/cdc.py
@@ -2,17 +2,45 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
-"""Class describing lint configuration object."""
+"""Class describing CDC configuration object."""
+
+import logging
+from collections.abc import Sequence
 
 from dvsim.flow.lint import LintCfg
+from dvsim.job.data import CompletedJobStatus
+from dvsim.tool.meridiancdc import cdc_parse_main
+from dvsim.utils import subst_wildcards
+
+# Get the global logger definition
+log = logging.getLogger(__name__)
 
 
 class CdcCfg(LintCfg):
-    """Derivative class for linting purposes."""
+    """Derivative class from LintCfg for parsing CDC logs."""
 
     flow = "cdc"
 
+    # Override the __init__ for a CDC run
+    # only names at the moment
     def __init__(self, flow_cfg_file, hjson_data, args, mk_config) -> None:
         super().__init__(flow_cfg_file, hjson_data, args, mk_config)
 
         self.results_title = f"{self.name.upper()} CDC Results"
+
+    # Override the _gen_results_for_cfg to generate the results.hsjon file
+    # As _gen_results_for_cfg expects the results.hjson to be there
+    # The results parsing is inserted before calling LintCfg _gen_results_for_cfg.
+    def _gen_results_for_cfg(self, results: Sequence[CompletedJobStatus]) -> None:
+        # Call the log parser for each build mode if it is not part of the build
+        # Note report_cmd can be added to the meridiancdc.hjson file if a local
+        # version of the parser is preferred and available.
+        if self.report_cmd is None or len(self.report_cmd) == 0:
+            for mode in self.build_modes:
+                result_path = subst_wildcards(self.build_dir, {"build_mode": mode.name})
+                result_file = f"{result_path}/results.hjson"
+                cdc_parse_main(["--repdir", result_path, "--outfile", result_file])
+        else:
+            log.info(f"For results.hjson Using external parser: {self.report_cmd}")
+
+        return super()._gen_results_for_cfg(results)
